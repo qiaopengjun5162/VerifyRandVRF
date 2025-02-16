01641c77ad518a95dd19b357f2a29598517f1630
QiaoPengjun5162 <qiaopengjun0@gmail.com>
2025-02-16 17:20:35 +0800

    feat: build: 添加合约部署日志并更新配置文件
    - 在 foundry.toml 中添加注释链接到相关合约地址
    - 修改 VerifyRandVRF.s.sol 脚本，添加合约部署日志输出
    - 引入环境变量 PRIVATE_KEY 用于部署合约

--- script/VerifyRandVRF.s.sol
+++ script/VerifyRandVRF.s.sol
@@ -12,12 +12,17 @@ contract VerifyRandVRFScript is Script {
     function setUp() public {}
 
     function run() public {
-        vm.startBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
 
         vrf = new VerifyRandVRF();
+        console.log("VerifyRandVRF deployed to: ", address(vrf));
+
         vrfFactory = new VerifyRandVRFFactory();
+        console.log("VerifyRandVRFFactory deployed to: ", address(vrfFactory));
 
         address proxyAddress = vrfFactory.createProxy(address(vrf), msg.sender);
+        console.log("Proxy deployed to: ", proxyAddress);
 
         vm.stopBroadcast();
     }
